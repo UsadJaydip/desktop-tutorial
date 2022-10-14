03c5675443432b56af97089152a9f7ff5722d5c1
UsadJaydip <114413179+UsadJaydip@users.noreply.github.com>
2022-10-14 18:47:17 +0530

    Address
    na

---
+++ Address.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	printf("\nName: \t\tusadjaydip");
+	printf("\nconatact no: \t6351184844");
+	printf("\nAdress: \tSardardham Veshnodevi Circle Ahemedabad");
+	printf("\nEmail ID: \tusadjaydip@gmail.com");
+	printf("\nDate Of Birth: \t06/10/2003");
+	printf("\nAge: \t\t 18");
+}
