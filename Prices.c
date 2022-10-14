f251759b29b900111ce40f2fe1d23c42059cb323
UsadJaydip <114413179+UsadJaydip@users.noreply.github.com>
2022-10-14 19:00:17 +0530

    save

---
+++ Prices.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	printf("\nName   : \tPrice");
+	printf("\nBanana : \t50");
+	printf("\nApple  : \t100");
+	printf("\nGrapes : \t150");
+	printf("\nPapaya : \t180");
+	printf("\nCusterd apple : 200");
+}
